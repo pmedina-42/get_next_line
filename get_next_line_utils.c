53f4a37382e9c7df4a058e4619bab7ed0666dd4d
Patricia Medina anglada <pmedina-@c2r9s3.42madrid.com>
2020-09-08 12:37:28 +0200

    gnl

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: pmedina- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/11 12:24:44 by pmedina-          #+#    #+#             */
-/*   Updated: 2020/09/01 12:32:22 by pmedina-         ###   ########.fr       */
+/*   Updated: 2020/09/08 12:33:06 by pmedina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,21 +56,22 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 
 	i = 0;
-	c2 = 0;
+	c2 = ft_strlen(s1);
 	c1 = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	if (!(x = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1)))
 		return (NULL);
-	while (c1 != ft_strlen(s1))
+	while (c1 != c2)
 	{
 		x[i] = s1[c1];
 		++i && ++c1;
 	}
-	while (c2 != ft_strlen(s2))
+	c2 = 0;
+	c1 = ft_strlen(s2);
+	while (c2 != c1)
 	{
-		x[i] = s2[c2];
-		++i && ++c2;
+		x[i++] = s2[c2++];
 	}
 	x[i] = '\0';
 	return (x);
